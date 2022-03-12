b5814b80ed6af9f179b535124d0a50e1a01ab2a7
Simon Levey <lev_47@hotmail.com>
2022-03-12 17:10:55 +0800

    JS highscore list render to local

--- assets/app.js
+++ assets/app.js
@@ -3,11 +3,21 @@ const sectionQuestion = document.getElementById("section-questions");
 const sectionLanding = document.getElementById("section-landing");
 const sectionTimer = document.getElementById("section-timer");
 const sectionInitials = document.getElementById("section-initials");
+const sectionHighscore = document.getElementById('section-highscore');
+
 const spanTime = document.getElementById("span-time");
 const questionTitle = document.getElementById("question-title");
 const spanFinalHighscore = document.getElementById("final-highscore");
 const questionChoices = document.getElementById("question-choices");
 const questionFeedback = document.getElementById('question-feedback');
+const formHighscore = document.getElementById('form-highscore');
+const inputInitials = document.getElementById('input-initials');
+
+const listHighscore = document.getElementById('list-highscore');
+
+const buttonPlayAgain = document.getElementById('button-play-again');
+const buttonClearHighscore = document.getElementById('button-clear-highscore');
+
 
 let timerId = null;
 let timeRemaining = 10;
@@ -54,6 +64,9 @@ function startTimer(){
         spanTime.textContent = timeRemaining
 
         //if time remaining < 0
+        
+        //what if timer expires?
+        //end game
         if(timeRemaining <= 0){
 
         // end game
@@ -126,14 +139,6 @@ function showQuestion(index){
 
 }
 
-
-
-
-//what if timer expires?
-//end game
-
-
-
 //end game
 function endGame(){
 
@@ -145,6 +150,8 @@ function endGame(){
 
     //hide Q's
     sectionQuestion.classList.add('hide');
+
+    sectionTimer.classList.add('hide');
 
 // 3. show high scores 
 // high score could be Q's answered correctly
@@ -158,6 +165,10 @@ function endGame(){
 // 1. user can type in the input box
 // do nothing ***check availability***
 
+
+formHighscore.addEventListener('submit', function(event){
+
+    event.preventDefault();
 // 2. user can hit enter in the input box
 // submit high score
 
@@ -165,17 +176,91 @@ function endGame(){
 // submit
 
 //submitting -- add the user initial and highscore - local storage
+const userInput = inputInitials.value;
+
+const highscore = {
+    name: userInput,
+    highscore: timeRemaining,
+
+}
+
+const existingHighscores = getHighscoresFromLocalStorage();
+//add in new highscore
+existingHighscores.push(highscore)
+//save it to local
+localStorage.setItem('highscores', JSON.stringify(existingHighscores));
+
 
 //after submit, redirect user to highscore page
+showHighscorePage();
+
+});
+
+/**
+ * 
+ * @returns {Array}
+ */
+function getHighscoresFromLocalStorage(){
+    return JSON.parse(localStorage.getItem('highscores') || '[]' )
+}
+
+function showHighscorePage(){
+    //hide endgame page
+    sectionInitials.classList.add('hide');
+
+    //show the highscore section
+    sectionHighscore.classList.remove('hide');
+    renderHighscoreList();
+}
+
+function renderHighscoreList(){
+    //Highscore page
+
+    //get all existing HS from local storage
+    const highscores = getHighscoresFromLocalStorage();
+
+    listHighscore.textContent = "";
+
+    //create a li on each item
+    for (let index = 0; index < highscores.length; index++) {
+        const highscore = highscores[index];
+        //chuck it in the list
+        const li = document.createElement('li');
+        
+        li.textContent = highscore.name + ' -- ' + highscore.highscore
+
+        listHighscore.appendChild(li);
+    }
+    
+
+}
 
 
-//Highscore page
+
+
 
 // 1. click on play again button
-// redirect user to landing page
+buttonPlayAgain.addEventListener('click', function(event){
+    // redirect user to landing page
+    window.location.reload();
+
+    })
 
 // 2. click on clear button
-// clear the local storage
-//clear the dom
+buttonClearHighscore.addEventListener('click', function(event){
+    // clear the local storage
+    localStorage.setItem('highscores', "[]");
+    //clear the dom
+    listHighscore.textContent = ""
 
-// show play again button
+    })
+
+
+
+
+    // show play again button
+
+
+
+
+
